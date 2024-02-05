db0471a2ead4f1ecad4a6f40a7af1a17a64d1980
szabiela <spencer@mufi.app>
2024-02-05 02:02:29 -0500

    trying javascript

---
+++ src/bash.js
@@ -0,0 +1,8 @@
+import Spline from '@splinetool/react-spline';
+
+export default function App() {
+  return (
+    <Spline scene="https://prod.spline.design/UXBwSxHjt9QnbTu0/scene.splinecode" />
+  );
+}
+
