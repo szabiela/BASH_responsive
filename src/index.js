50c9e6fd6acdff0dcdf67b823e2592967789ea51
szabiela <spencer@mufi.app>
2024-02-20 13:55:55 -0500

    sz update

--- src/index.js
+++ src/index.js
@@ -34,6 +34,3 @@ canvas.addEventListener("mousedown", (e) => {
 canvas.addEventListener("mousemove", draw);
 canvas.addEventListener("mouseup", () => (isDrawing = false));
 canvas.addEventListener("mouseout", () => (isDrawing = false));
-
-
-
